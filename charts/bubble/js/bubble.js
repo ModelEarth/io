064a9a46a6be10bd5e17e5b8f504f3e871e3ba36
modelearth <model@dreamstudio.com>
2024-11-01 17:51:11 -0400

    Change to 2.0.1-411

--- charts/bubble/js/bubble.js
+++ charts/bubble/js/bubble.js
@@ -166,7 +166,7 @@ function refreshBubbleWidget() {
     priorHash_bubble = getHash();
 }
 
-const indicatorsUrl = "/io/build/api/USEEIOv2.0/indicators.json";
+const indicatorsUrl = "/io/build/api/USEEIOv2.0.1-411/indicators.json";
 let indicators = new Set();
 let dropdownX = $("#graph-picklist-x");
 let dropdownY = $("#graph-picklist-y");
@@ -241,7 +241,7 @@ function updateTitle(x,y,z) {
   var unitx, unity, unitz;
   console.log("updateTitle " + x + " " + y + " " + z);
   //let params = loadParams(location.search,location.hash);
-  d3.json("/io/build/api/USEEIOv2.0/indicators.json").then(function(consdata){
+  d3.json("/io/build/api/USEEIOv2.0.1-411/indicators.json").then(function(consdata){
     console.log("attempt");
     var filteredData = consdata.filter(function(d) {
       if (d["id"]==x) {
